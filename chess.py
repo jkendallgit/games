95772fdb34cdf4b9d2ec38a534fafb7fdbbb3f96
jkendallgitusr <jkendallgit@gmail.com>
2019-09-21 17:23:19 -0400

    updates....

--- chess.py
+++ chess.py
@@ -44,9 +44,9 @@ def get_moves(move_type, board, row_position, col_position):
         for idx in range(10):
             print("LOOK IDX IS: " + str(idx))
             print("LOOK ROW POSITION IS: " + str(row_position))
-            if (idx == row_position):
-                print("LOOK, IDX IS ROW POSITION, IDX IS: " + str(idx))
-             #   possible_moves.append(idx)
+            if (idx != row_position):
+                print("NOOOOOO LOOK, IDX IS NOT ROW POSITION, IDX IS: " + str(idx))
+                possible_moves.append(idx)
                 #print("idx: " + str(idx))
                 #print("row: " + str(row))
     #else:
