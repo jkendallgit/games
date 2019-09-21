4d62d2b7027299e8411eec19e7e1d377bb02399e
jkendallgitusr <jkendallgit@gmail.com>
2019-09-20 21:08:24 -0400

    updates..

--- test.py
+++ test.py
@@ -10,7 +10,19 @@ mylist = [[" ", "  a", "   b", "   c", "   d", "   e", "   f", "   g", "   h"],
 ["2", " ", " ", " ", " ", " ", " ", " ", " "],
 ["1", " ", " ", " ", " ", " ", " ", " ", " "]]
 
-mylist[2][5] = "K"
+# -piece KNIGHT -position d2
+mylist[7][4] = "K" #d2
+
+# Next step, given an input coordinate like "d2", Draw a "K" as an item at that location within the list matrix.
+#d2
+print(mylist[7][4])
+8 => 1
+7 => 2
+
+#Do mappingin 2 steps, 2nd param passed in, like "2" will map to the "7th item in first (row) list,
+# and then the 1st param that got passed in, like "d" will map to the 4th item in second (col) list.
+
+
 
 # Draw Chess Board (this needs to turn into a function)
 for i in range(len(mylist)):
