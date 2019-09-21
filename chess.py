2b65d23ad682c10fe7503779f75f91f771f5445c
jkendallgitusr <jkendallgit@gmail.com>
2019-09-21 06:38:14 -0400

    updates..

--- chess.py
+++ chess.py
@@ -1,3 +1,13 @@
+import argparse
+
+# Parse user input
+parser = argparse.ArgumentParser()
+parser.add_argument('-piece', required=True)
+parser.add_argument('-position', required=True)
+args = parser.parse_args()
+input_piece = args.piece
+input_position = args.position
+
 # Draw Chess Board (this needs to turn into a function)
 def print_board(board):
     for i in range(len(board)):
@@ -11,13 +21,12 @@ def print_board(board):
         print()
 
 def get_position(r_heading, c_heading):
-    # row heading/ outer list item position
+    # row heading/ outer list position
     row_mapping = {'1': 8, '2': 7, '3': 6, '4': 7, '5': 4, '6': 3, '7': 3, '8': 1 }
-    # col heading/ inner list item position
+    # col heading/ inner list position
     col_mapping = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8 }
     row_list_pos = row_mapping[r_heading]
     col_list_pos = col_mapping[c_heading]
-    #print("Player is at " + r_heading + c_heading + " which is " + str(row_list_pos) + "," + str(col_list_pos))
     return row_list_pos, col_list_pos
 
 # Initialize chess board
@@ -31,20 +40,20 @@ board = [[" ", "  a", "   b", "   c", "   d", "   e", "   f", "   g", "   h"],
 ["2", " ", " ", " ", " ", " ", " ", " ", " "],
 ["1", " ", " ", " ", " ", " ", " ", " ", " "]]
 
-# print initialized board...
-print_board(board)
 
-# User input would be 2d...
+# input_piece  
+# input_position  
 
 # given user input, call get_position to return the outer and inner list locations...
-# position = get_position("2", "d")
 row_position, col_position = get_position("2", "d")
 
 print("Row Position is: " + str(row_position))
 print("Col Position is: " + str(col_position))
 
-# -piece KNIGHT -position d2
-board[row_position][col_position] = "K"
+# chess.py -piece KNIGHT -position d2
+# board[row_position][col_position] = "K"
+board[row_position][col_position] = input_piece
 
 # re-draw the board...
 print_board(board)
+
