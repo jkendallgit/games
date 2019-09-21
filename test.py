af4e104f569468e2c77ceb6284fb2de7884d733b
jkendallgitusr <jkendallgit@gmail.com>
2019-09-21 05:55:28 -0400

    updates..

--- test.py
+++ test.py
@@ -10,16 +10,14 @@ def print_board(board):
                 print(board[i][j], end = ' ')
         print()
 
-def get_position(row, col):
+def get_position(r_heading, c_heading):
     # row heading/ outer list item position
-    row_heading = {'1': 8, '2': 7, '3': 6, '4': 7, '5': 4, '6': 3, '7': 3, '8': 1 }
-    print(row_heading['3'])
-
+    row_mapping = {'1': 8, '2': 7, '3': 6, '4': 7, '5': 4, '6': 3, '7': 3, '8': 1 }
     # col heading/ inner list item position
-    col_heading = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8 }
-    print(col_heading['c'])
- 
-    print("hi from get_position: " + row + col)
+    col_mapping = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8 }
+    row_list_pos = row_mapping[r_heading]
+    col_list_pos = col_mapping[c_heading]
+    print("Player is at " + r_heading + c_heading + " which is " + str(row_list_pos) + "," + str(col_list_pos))
 
 # Initialize chess board
 
