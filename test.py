b3038494fa6bf401938f92a70bdac70adeadee6d
jkendallgitusr <jkendallgit@gmail.com>
2019-09-20 20:28:37 -0400

    updates..

--- test.py
+++ test.py
@@ -1,36 +1,19 @@
+mylist = [[" ", "  a", "   b", "   c", "   d", "   e", "   f", "   g", "   h"],
+["8", " ", " ", " ", " ", " ", " ", " ", " "],
+["7", " ", " ", " ", " ", " ", " ", " ", " "],
+["6", " ", " ", " ", " ", " ", " ", " ", " "],
+["5", " ", " ", " ", " ", " ", " ", " ", " "],
+["4", " ", " ", " ", " ", " ", " ", " ", " "],
+["3", " ", " ", " ", " ", " ", " ", " ", " "],
+["2", " ", " ", " ", " ", " ", " ", " ", " "],
+["1", " ", " ", " ", " ", " ", " ", " ", " "]]
 
-mylist = [["8", "x", "x", "x", "x", "x", "x", "x", "x"],
-["7", "x", "x", "x", "x", "x", "x", "x", "x"],
-["6", "x", "x", "x", "x", "x", "x", "x", "x"],
-["5", "x", "x", "x", "x", "x", "x", "x", "x"],
-["4", "x", "x", "x", "x", "x", "x", "x", "x"],
-["3", "x", "x", "x", "x", "x", "x", "x", "x"],
-["2", "x", "x", "x", "x", "x", "x", "x", "x"],
-["1", "x", "x", "x", "x", "x", "x", "x", "x"]]
-
-# print("  a b c d e f g h")
 for i in range(len(mylist)):
+    if i > 1:
+        print("   ---  ---  ---  ---  ---  ---  ---  ---")
     for j in range(len(mylist[i])):
-        #if j >= 1:
+        if i > 0 and j > 0:
+            print(" |" + mylist[i][j] + "|", end = ' ')
+        else:
             print(mylist[i][j], end = ' ')
-        #else:
-            #print(" " + mylist[i][j], end = ' ')
     print()
-
-
-# mylist = [["x"] * 8 for i in range(8)]
-# # print(mylist[1][2])
-#for item in mylist:
-#    print(str(item), end = '*')
-
-# for x,y in mylist:
-#    print(x,y)
-
-#for i in mylist:
-#    print(i[0], i[1])
-# Printing list using map 
-# print ("List in proper method", '[%s]' % ', '.join(map(str, mylist))) 
-
-#print(mylist)
-#for item in range(8):
-#    print(chessBoard[i])    
