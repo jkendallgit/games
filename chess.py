226e83c6bb7db4180edb406fa85ee6e44bfa7c6d
jkendallgitusr <jkendallgit@gmail.com>
2019-09-28 09:56:23 -0400

    updates

--- chess.py
+++ chess.py
@@ -20,24 +20,23 @@ def print_board(board):
                 print(board[i][j], end = ' ')
         print()
 
-# This function translates list positions to -> algebraic positions 
+# This function translates algebraic positions to ->  list positions 
 def get_list_position(r_heading, c_heading):
-    # row heading/ outer list position
+    # r_heading is outer list position
     row_mapping = {'1': 8, '2': 7, '3': 6, '4': 5, '5': 4, '6': 3, '7': 2, '8': 1 }
-    # col heading/ inner list position
+    # c_heading is inner list position
     col_mapping = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8 }
     row_list_pos = row_mapping[r_heading]
     col_list_pos = col_mapping[c_heading]
     return row_list_pos, col_list_pos
 
-# Use this function to return back algebraic location of possible moves to user...
-def get_board_position(r_heading, c_heading):
+# This function translates list positions to -> algebraic positions
+def get_alg_position(r_heading, c_heading):
     row_mapping = {8: 1, 7: 2, 6: 3, 5: 4, 4: 5, 3: 6, 2: 7, 1: 8 }
     col_mapping = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h' }
     row_list_pos = row_mapping[r_heading]
     col_list_pos = col_mapping[c_heading]
     board_position =  str(col_list_pos) + str(row_list_pos)
-    # print("HELLO FROM get_board_positions: board_position: " + board_position)
     return board_position
 
 def get_moves(move_type, board, row_position, col_position):
@@ -76,7 +75,6 @@ board = [[" ", "  a", "   b", "   c", "   d", "   e", "   f", "   g", "   h"],
 row_position, col_position = get_list_position(input_position[1], input_position[0])
 board[row_position][col_position] = input_piece
 
-
 # execute h moves
 h_moves = get_moves("h", board, row_position, col_position)
 h_move_positions = []
@@ -85,17 +83,18 @@ for move in h_moves:
     list_position = int(str(row_position) + str(move))
     h_move_positions.append(list_position)
 
-board_positions = []
-for id, rp in enumerate(h_move_positions):
-    row_p = int(str(rp)[0])
-    col_p = int(str(rp)[1])
-    board_positions.append(get_board_position(row_p, col_p))
-# print("Look here are board_positions: " + str(board_positions))
-   
 # execute v moves
 v_moves = get_moves("v",board, row_position, col_position)
 for move in v_moves:
     board[move][col_position] = "v"
+
+board_positions = []
+for id, rp in enumerate(h_move_positions):
+    row_p = int(str(rp)[0])
+    col_p = int(str(rp)[1])
+    board_positions.append(get_alg_position(row_p, col_p))
+   
+
 
 # LOOK
 # work on this next, you want to append all the  possible v moves to board_positions list,
@@ -103,10 +102,9 @@ for move in v_moves:
 # for id, rp in enumerate(h_move_positions):
 #    row_p = int(str(rp)[0])
 #    col_p = int(str(rp)[1])
-#    board_positions.append(get_board_position(row_p, col_p))
+#    board_positions.append(get_alg_position(row_p, col_p))
 # print("Look here are board_positions: " + str(board_positions))
 
-print_board(board)
-
+# print_board(board)
 
 print("Look here are board_positions: " + str(board_positions))
